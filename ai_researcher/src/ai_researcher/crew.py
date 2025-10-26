21d1fd0a91a9366a86e1f9764a156bde87e61b9f
Lingchen Zhu <zhulingchen@gmail.com>
2025-10-26 10:39:37 -0400

    Use the CrewAI tool `SerperDevTool`

--- ai_researcher/src/ai_researcher/crew.py
+++ ai_researcher/src/ai_researcher/crew.py
@@ -1,6 +1,7 @@
 from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
 from typing import List
 
 @CrewBase
@@ -10,30 +11,35 @@ class AiResearcher():
     agents: List[BaseAgent]
     tasks: List[Task]
 
+    def __init__(self):
+        serper_tool = SerperDevTool()
+        self.tools = [serper_tool]
+
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config['researcher'],
+            verbose=True,
+            tools=self.tools,
         )
 
     @agent
     def reporter(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporter'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config['reporter'],
+            verbose=True,
         )
 
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['research_task'],
         )
 
     @task
     def reporting_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            config=self.tasks_config['reporting_task'],
             output_file='outputs/report.md'
         )
 
